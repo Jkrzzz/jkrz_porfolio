0ac3a56b3f3bfe483e99dd93e6714746351c7e19
Jkrzzz <heinhtetjkrz@gmail.com>
2023-03-22 15:31:13 +0630

    LoadingScreen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import ClockLoader from "react-spinners/ClockLoader";
 import "./App.css";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
@@ -12,8 +14,28 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Work from "./components/work/Work";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  const checkDark = localStorage.getItem("selectedTheme");
+  const color = checkDark === "light" ? "#123421" : "#efefef";
   return (
     <>
+      {loading && (
+        <div className="loading-react">
+          <ClockLoader
+            color={color}
+            loading={loading}
+            size={250}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      )}
       <Header />
       <main className="main">
         <Home />
